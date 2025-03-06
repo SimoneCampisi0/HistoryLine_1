2bcb6928fddfd8c0c956ff97940597a9e1f5461d
Simone Campisi <126571480+SimoneCampisi0@users.noreply.github.com>
2025-03-06 18:20:33 +0100

    Add menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
+import Menu from "./components/Menu.jsx";
 
 function App() {
 
     return (<>
             <div className="w-100 min-vh-100 bg-black">
+                <div className="w-75 mx-auto">
+                    <Menu/>
+                </div>
             </div>
         </>)
 }
 
-export default App
+export default App;
---
+++ src/components/Menu.jsx
@@ -0,0 +1,16 @@
+function Menu() {
+
+    return (
+        <>
+            <div className="d-flex flex-row justify-content-between p-4 text-white">
+                <div className="">HistoryLine</div>
+                <div className="d-flex flex-row justify-content-between gap-5">
+                    <div>Home</div>
+                    <div>About</div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Menu;
