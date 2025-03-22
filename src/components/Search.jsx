bbfc8fd4bdb1df2d3cb91035ce5f52ecdc4c98e1
Simone Campisi <126571480+SimoneCampisi0@users.noreply.github.com>
2025-03-22 19:18:43 +0100

    add onSearchButton

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,15 +1,31 @@
+import {useState} from "react";
+
 function Search() {
+    const [searchText, setSearchText] = useState('');
+
     function onSearchButton () {
-        console.log("onSearchButton")
+        console.log("searchText: ", searchText);
+    }
+
+    function onChangeText (value) {
+        console.log("onChangeText: ", value);
+        setSearchText(value)
     }
 
     return (
         <>
-            <div className="d-flex flex-column justify-content-center align-items-center p-4 gap-4 text-white">
+            <div className="form-group d-flex flex-column justify-content-center align-items-center p-4 gap-4 text-white">
                 <div className="h2">Discovery life of a historical character!</div>
                 {/*Modificare width con la lunghezza del div sopra*/}
-                <input style={{ padding: "0.50rem", width: 552 }} className="border rounded" type="text" />
-                <button onClick={onSearchButton}>Search</button>
+                <input
+                    style={{ width: 552 }}
+                    className="form-control"
+                    type="text"
+                    placeholder="Search historical character..."
+                    value={searchText}
+                    onChange={e => onChangeText(e.target.value)}
+                />
+                <button className="btn btn-dark" onClick={onSearchButton}>Search</button>
             </div>
         </>
     )
