e66cecd57f64ecefecf61c6f1a2e96cd569a11ca
Simone Campisi <126571480+SimoneCampisi0@users.noreply.github.com>
2025-05-02 17:50:16 +0200

    Add logic to Timeline

--- src/components/Timeline/Timeline.jsx
+++ src/components/Timeline/Timeline.jsx
@@ -1,16 +1,18 @@
 import './Timeline.css';
 import dayjs from "dayjs";
-import 'dayjs/locale/it.js'
+import 'dayjs/locale/en.js'
 import PropTypes from "prop-types";
-dayjs.locale('it');
+dayjs.locale('en');
 
 function Timeline({ resultEvents }) {
+    //TODO: aggiungere supporto date AC e DC
     return (
         <>
             <section className="py-5">
                 <ul className="timeline">
                     {resultEvents.events.map((item, index) => {
-                        const formattedDate = dayjs(item.eventDate).format('D MMMM YYYY');
+                        const date = dayjs(item.eventDate);
+                        const formattedDate = `${date.date()} ${date.format('MMMM')} ${date.year()}`;
                         return (
                             <li className="timeline-item mb-5" key={index}>
                                 <h5 className="fw-bold text-white">{item.eventName}</h5>
