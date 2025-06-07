547b40aeb017116bbe98dc95dd560069fef23e89
Simone Campisi <126571480+SimoneCampisi0@users.noreply.github.com>
2025-06-07 17:19:57 +0200

    Aggiunto menu mobile

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,6 +3,8 @@ import PropTypes from "prop-types";
 import {useState} from "react";
 import ReactFlagsSelect from "react-flags-select";
 import {useNavigate} from "react-router-dom";
+import { Offcanvas } from 'bootstrap';
+
 function Menu({ language, setLanguage }) {
     const navigate = useNavigate();
     const [languageSelected, setLanguageSelected] = useState("IT");
@@ -21,39 +23,78 @@ function Menu({ language, setLanguage }) {
 
     function goTo(path) {
         navigate(path);
+        const element = document.getElementById('mobileMenu');
+        const offcanvas = Offcanvas.getInstance(element);
+        offcanvas?.hide();
     }
 
     return (
         <>
-            <div className="container d-flex flex-row justify-content-between align-items-center p-4 text-white h4">
-                <div className="" role="button">HistoryLine</div>
-                <div className="d-flex flex-row align-items-center gap-4">
-                    <div className="d-none d-md-flex flex-row gap-4">
-                        {language === 'italian' &&
-                            <>
-                                <div onClick={() => goTo('/')} role="button">Home</div>
-                                <div onClick={() => goTo('/about')} role="button">Chi siamo</div>
-                            </>
-                        }
-                        {language === 'english' &&
-                            <>
-                                <div onClick={() => goTo('/')} role="button">Home</div>
-                                <div onClick={() => goTo('/about')} role="button">About</div>
-                            </>
-                        }
+            <div className="container p-4 text-white h4">
+                <div className="row d-flex justify-content-center align-items-center">
+                    <div className="col-6">
+                        <div className="navbar-brand" role="button">HistoryLine</div>
                     </div>
+                    <div className="col-6">
 
-                    <ReactFlagsSelect
-                        selectButtonClassName="custom-react-flag"
-                        selected={languageSelected}
-                        onSelect={onChangeLanguage}
-                        countries={["IT", "GB"]}
-                        showSelectedLabel={false}
-                        showSecondarySelectedLabel={false}
-                        showOptionLabel={false}
-                        fullWidth={false}
-                    />
 
+                        {/* Menu */}
+                        <div className="d-flex justify-content-end gap-3">
+                            <button className="navbar-toggler d-md-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#mobileMenu"
+                                    aria-controls="mobileMenu" aria-label="Toggle navigation">
+                                <span className="navbar-toggler-icon"></span>
+                            </button>
+
+                            <div className="offcanvas offcanvas-top text-bg-dark d-md-none" tabIndex="-1" id="mobileMenu"
+                                 aria-labelledby="mobileMenuLabel">
+                                <div className="offcanvas-header">
+                                    <h5 className="offcanvas-title" id="mobileMenuLabel">Menu</h5>
+                                    <button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                                </div>
+                                <div className="offcanvas-body d-flex flex-column gap-3 fs-5">
+                                    {language === 'italian' ? (
+                                        <>
+                                            <div role="button" onClick={() => goTo('/')}>Home</div>
+                                            <div role="button" onClick={() => goTo('/about')}>Chi siamo</div>
+                                        </>
+                                    ) : (
+                                        <>
+                                            <div role="button" onClick={() => goTo('/')}>Home</div>
+                                            <div role="button" onClick={() => goTo('/about')}>About</div>
+                                        </>
+                                    )}
+                                </div>
+                            </div>
+
+
+                            <div className="d-none d-md-flex flex-row ">
+                                {language === 'italian' &&
+                                    <>
+                                        <div className="m-2" onClick={() => goTo('/')} role="button">Home</div>
+                                        <div className="m-2" onClick={() => goTo('/about')} role="button">Chi siamo</div>
+                                    </>
+                                }
+                                {language === 'english' &&
+                                    <>
+                                        <div className="m-2"  onClick={() => goTo('/')} role="button">Home</div>
+                                        <div className="m-2"  onClick={() => goTo('/about')} role="button">About</div>
+                                    </>
+                                }
+                            </div>
+
+                            <ReactFlagsSelect
+                                selectButtonClassName="custom-react-flag"
+                                selected={languageSelected}
+                                onSelect={onChangeLanguage}
+                                countries={["IT", "GB"]}
+                                showSelectedLabel={false}
+                                showSecondarySelectedLabel={false}
+                                showOptionLabel={false}
+                                fullWidth={false}
+                            />
+
+                        </div>
+                    </div>
                 </div>
             </div>
 
